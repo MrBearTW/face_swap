fc024f4768bb880d6da60307bfe152fb59c1b73a
Rudy Chin <bb1168kk@gmail.com>
2017-06-23 18:06:00 +0800

    fit container fs

--- py_face_swap/pytest.py
+++ py_face_swap/pytest.py
@@ -4,16 +4,16 @@ import sys
 import time
 
 def main():
-    landmarks = '/data/rudychin/face-swap/data/models/shape_predictor_68_face_landmarks.dat'       # path to landmarks model file
-    model_3dmm_h5 = '/data/rudychin/face-swap/data/models/BaselFaceModel_mod_wForehead_noEars.h5'  # path to 3DMM file (.h5)
-    model_3dmm_dat = '/data/rudychin/face-swap/data/models/BaselFace.dat'                          # path to 3DMM file (.dat)
-    reg_model = '/data/rudychin/face-swap/data/models/3dmm_cnn_resnet_101.caffemodel'              # path to 3DMM regression CNN model file (.caffemodel)
-    reg_deploy = '/data/rudychin/face-swap/data/models/3dmm_cnn_resnet_101_deploy.prototxt'        # path to 3DMM regression CNN deploy file (.prototxt)
-    reg_mean = '/data/rudychin/face-swap/data/models/3dmm_cnn_resnet_101_mean.binaryproto'         # path to 3DMM regression CNN mean file (.binaryproto)
-    seg_model = '/data/rudychin/face-swap/data/models/face_seg_fcn8s.caffemodel'                   # path to face segmentation CNN model file (.caffemodel)
-    seg_deploy = '/data/rudychin/face-swap/data/models/face_seg_fcn8s_deploy.prototxt'             # path to face segmentation CNN deploy file (.prototxt)
-    source = '/data/rudychin/face-swap/data/images/brad_pitt_01.jpg'     # source image
-    target = '/data/rudychin/face-swap/data/images/795.jpg'  # target image
+    landmarks = '/root/face_swap/data/models/shape_predictor_68_face_landmarks.dat'       # path to landmarks model file
+    model_3dmm_h5 = '/root/face_swap/data/models/BaselFaceModel_mod_wForehead_noEars.h5'  # path to 3DMM file (.h5)
+    model_3dmm_dat = '/root/face_swap/data/models/BaselFace.dat'                          # path to 3DMM file (.dat)
+    reg_model = '/root/face_swap/data/models/3dmm_cnn_resnet_101.caffemodel'              # path to 3DMM regression CNN model file (.caffemodel)
+    reg_deploy = '/root/face_swap/data/models/3dmm_cnn_resnet_101_deploy.prototxt'        # path to 3DMM regression CNN deploy file (.prototxt)
+    reg_mean = '/root/face_swap/data/models/3dmm_cnn_resnet_101_mean.binaryproto'         # path to 3DMM regression CNN mean file (.binaryproto)
+    seg_model = '/root/face_swap/data/models/face_seg_fcn8s.caffemodel'                   # path to face segmentation CNN model file (.caffemodel)
+    seg_deploy = '/root/face_swap/data/models/face_seg_fcn8s_deploy.prototxt'             # path to face segmentation CNN deploy file (.prototxt)
+    source = '/root/face_swap/data/images/brad_pitt_01.jpg'     # source image
+    target = '/root/face_swap/data/images/795.jpg'  # target image
 
     fs = pyfaceswap.PyFaceSwap()
     if( fs.initCtx(len(sys.argv), sys.argv) ):
@@ -24,6 +24,9 @@ def main():
 
     sourceImg = cv2.imread(source)
     targetImg = cv2.imread(target)
+
+    sourceImg = cv2.resize(sourceImg, (240,240))
+    targetImg = cv2.resize(targetImg, (240,240))
     
     result = []
 
@@ -44,7 +47,7 @@ def main():
     latency = (time.time() - start) / 5.0
     print latency
 
-    cv2.imwrite('test.jpg', result)
+    cv2.imwrite('/root/face_swap/data/outptu/test.jpg', result)
 
     del fs
 
