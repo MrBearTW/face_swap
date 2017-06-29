5c11431a10bbe330654d9ffe55247adea61ffe94
Rudy Chin <bb1168kk@gmail.com>
2017-06-29 15:44:19 +0800

    A working version of FsProcessor

--- py_face_swap/fs_service.py
+++ py_face_swap/fs_service.py
@@ -1,28 +1,77 @@
 import argparse
 import random
-
+import sys
+import cv2
 import pyfaceswap
+import numpy as np
 from ailabs.jarvis import Processor
+from PIL import Image
 
-class FS_Processor(Processor):
+class FsProcessor(Processor):
 
     def __init__(self):
-        super(MyProcessor, self).__init__()
+        super(FsProcessor, self).__init__()
 
     def on_start(self, session_id):
         print ">>> Processor [Video dummy: metadata] started: %d <<<" % (session_id, )
+        self.counter = 0
+        self.lastRes = None # (PIL, metadata)
+
+
+        self.fs = pyfaceswap.PyFaceSwap()
+        if( self.fs.initCtx(len(sys.argv), sys.argv) ):
+            print 'Initialization failed!'
+
+        gpuId = 1
+        expReg = 1
+        genericFace = 0
+        highQual = 0
+        self.fs.loadModels(landmarks, model_3dmm_h5, model_3dmm_dat, reg_model, reg_deploy,\
+                reg_mean, seg_model, seg_deploy, genericFace, expReg, highQual, gpuId)
+
+        if highQual:
+            self.targetHeight = 240.0
+        else:
+            self.targetHeight = 200.0
+
+        sourceImg = cv2.imread(source)
+        srcRszRatio = self.targetHeight / sourceImg.shape[0]
+        rszSrcImg = cv2.resize(sourceImg, None, None, fx=srcRszRatio, fy=srcRszRatio,\
+                interpolation=cv2.INTER_LINEAR)
+        if ( self.fs.setSourceImg(rszSrcImg) ):
+            print 'Set Source Image Failed!'
+        print '>>> Source set!'
+
 
     def on_end(self, session_id):
         print ">>> Processor ended: %d <<<" % (session_id, )
 
-    def on_image_frame(self, session_id, stream_id, time, image_frame, metadata):        
-        # do algorithm processing
-        if ( fs.setTargetImg(image_frame) ):
-            print 'Set Source Image Failed!'
-            return
-        result = fs.swap()
+    def pil2cv(self, img):
+        cvImg = np.array(img)
+        cvImg = cv2.cvtColor(cvImg, cv2.COLOR_RGB2BGR)
+        return cvImg
 
-        self.send_image_frame(None, metadata)
+
+    def on_image_frame(self, session_id, stream_id, time, image_frame, metadata):        
+        if self.counter == 0:
+            self.lastRes = (image_frame, metadata)
+            #Convert image from PIL to ndarray
+            cvTgtImage = self.pil2cv(image_frame)
+            tgtRszRatio = self.targetHeight / cvTgtImage.shape[0]
+            rszTgtImg = cv2.resize(cvTgtImage, None, None, fx=tgtRszRatio, fy=tgtRszRatio,\
+                    interpolation=cv2.INTER_LINEAR)
+            if ( self.fs.setTargetImg(rszTgtImg) ):
+                print 'Set Target Image Failed! Use last result'
+            tmp = self.fs.swap()
+            rszRes = cv2.resize(tmp, image_frame.size, interpolation=cv2.INTER_LINEAR)
+            rszRes = cv2.cvtColor(rszRes, cv2.COLOR_BGR2RGB)
+            self.lastRes = (Image.fromarray(rszRes), metadata)
+        elif self.counter == 5:
+            for _ in range(self.counter+1):
+                self.send_image_frame(self.lastRes[0], self.lastRes[1])
+            self.counter = -1
+        self.counter = self.counter + 1
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -38,29 +87,8 @@ if __name__ == '__main__':
     seg_model = '/root/face_swap/data/models/face_seg_fcn8s.caffemodel'                   # path to face segmentation CNN model file (.caffemodel)
     seg_deploy = '/root/face_swap/data/models/face_seg_fcn8s_deploy.prototxt'             # path to face segmentation CNN deploy file (.prototxt)
     source = '/root/face_swap/data/images/brad_pitt_01.jpg'     # source image
+    target = '/root/face_swap/data/images/trump.avi'     # source image
+    cap = cv2.VideoCapture(target)
 
-    fs = pyfaceswap.PyFaceSwap()
-    if( fs.initCtx(len(sys.argv), sys.argv) ):
-        print 'Initialization failed!'
-        return
-    fs.loadModels(landmarks, model_3dmm_h5, model_3dmm_dat, reg_model, reg_deploy,\
-            reg_mean, seg_model, seg_deploy, 0, 1, 1)
-
-    sourceImg = cv2.imread(source)
-    if ( fs.setSourceImg(sourceImg) ):
-        print 'Set Source Image Failed!'
-        return
-
-    MyProcessor.startToListen(args.port)
-
-
-
-
-
-
-
-
-
-
-
+    FsProcessor.startToListen(args.port)
 
