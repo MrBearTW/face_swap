968aa72aaa50921a23111aeac18be4579ae13088
Rudy Chin <bb1168kk@gmail.com>
2017-07-13 09:42:28 +0800

    add new dfm model

--- py_face_swap/fs_service.py
+++ py_face_swap/fs_service.py
@@ -128,8 +128,6 @@ if __name__ == '__main__':
     landmarks = '/root/face_swap/data/models/shape_predictor_68_face_landmarks.dat'       # path to landmarks model file
     model_3dmm_h5 = '/root/face_swap/data/models/BaselFaceModel_mod_wForehead_noEars.h5'  # path to 3DMM file (.h5)
     model_3dmm_dat = '/root/face_swap/data/models/BaselFace.dat'                          # path to 3DMM file (.dat)
-    reg_model = '/root/face_swap/data/models/dfm_resnet_101.caffemodel'              # path to 3DMM regression CNN model file (.caffemodel)
-    reg_deploy = '/root/face_swap/data/models/dfm_resnet_101_deploy.prototxt'        # path to 3DMM regression CNN deploy file (.prototxt)
     reg_mean = '/root/face_swap/data/models/dfm_resnet_101_mean.binaryproto'         # path to 3DMM regression CNN mean file (.binaryproto)
     seg_model = '/root/face_swap/data/models/face_seg_fcn8s.caffemodel'                   # path to face segmentation CNN model file (.caffemodel)
     seg_deploy = '/root/face_swap/data/models/face_seg_fcn8s_deploy.prototxt'             # path to face segmentation CNN deploy file (.prototxt)
@@ -155,13 +153,17 @@ if __name__ == '__main__':
     genericFace = 0
     highQual = args.highQual
     if highQual:
-        print 'High Quality Enabled!'
+        print 'High Quality Enabled! (360p, ResNet-101)'
         targetHeight = 360.0
         skipped = 0
+        reg_model = '/root/face_swap/data/models/dfm_resnet_101.caffemodel'              # path to 3DMM regression CNN model file (.caffemodel)
+        reg_deploy = '/root/face_swap/data/models/dfm_resnet_101_deploy.prototxt'        # path to 3DMM regression CNN deploy file (.prototxt)
     else:
-        print 'Low Quality Enabled!'
+        print 'Low Quality Enabled! (180p, VGG16)'
         targetHeight = 180.0
         skipped = 0
+        reg_model = '/root/face_swap/data/models/dfm_vgg16.caffemodel'              # path to 3DMM regression CNN model file (.caffemodel)
+        reg_deploy = '/root/face_swap/data/models/dfm_vgg16_deploy.prototxt'        # path to 3DMM regression CNN deploy file (.prototxt)
 
 
     renderThread = Renderer()
