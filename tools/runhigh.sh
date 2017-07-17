815ccb020fb962f4879d55bb3be0f32a5a614b8a
Rudy Chin <bb1168kk@gmail.com>
2017-07-17 10:30:40 +0800

    update script location

--- tools/runhigh.sh
+++ tools/runhigh.sh
@@ -3,8 +3,8 @@ for idol in 2 3 4
 do
     for imgH in 640
     do
-        python py_face_swap/swapVideo.py -i data/images/rotation.MOV -o data/output/fsbench/rotation_kcf_${imgH}_high_${idol}.avi --idol ${idol} --gpu 1 --rotate 1 --highQual 1 --imgH ${imgH}
-        python py_face_swap/swapVideo.py -i data/images/expression.MOV -o data/output/fsbench/expression_kcf_${imgH}_high_${idol}.avi --idol ${idol} --gpu 1 --rotate 1 --highQual 1 --imgH ${imgH}
-        python py_face_swap/swapVideo.py -i data/images/segmentation.MOV -o data/output/fsbench/segmentation_kcf_${imgH}_high_${idol}.avi --idol ${idol} --gpu 1 --rotate 1 --highQual 1 --imgH ${imgH}
+        python swapVideo.py -i data/images/rotation.MOV -o data/output/fsbench/rotation_kcf_${imgH}_high_${idol}.avi --idol ${idol} --gpu 1 --rotate 1 --highQual 1 --imgH ${imgH}
+        python swapVideo.py -i data/images/expression.MOV -o data/output/fsbench/expression_kcf_${imgH}_high_${idol}.avi --idol ${idol} --gpu 1 --rotate 1 --highQual 1 --imgH ${imgH}
+        python swapVideo.py -i data/images/segmentation.MOV -o data/output/fsbench/segmentation_kcf_${imgH}_high_${idol}.avi --idol ${idol} --gpu 1 --rotate 1 --highQual 1 --imgH ${imgH}
     done
 done
